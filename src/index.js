060d8f9b09a043b669a41774e33b77bac35dfc63
Hamish Hall <hachall@hotmail.com>
2018-03-15 00:00:28 +0000

    flat components

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import Hello from './components/hello';
+// import Hello from './components/hello';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Hello />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
