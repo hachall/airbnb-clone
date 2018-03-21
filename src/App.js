d61b141a5e9c2f7969fb74c4e3e69fcb0d2744b4
Hamish Hall <hachall@hotmail.com>
2018-03-21 10:25:32 +0000

    markers and search filter

--- src/App.js
+++ src/App.js
@@ -3,12 +3,16 @@ import GoogleMapReact from 'google-map-react';
 // import logo from './logo.svg';
 import './App.css';
 import Flat from './components/flat'
+import Marker from './components/marker'
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      flats: []
+      flats: [],
+      allFLats: [],
+      selectedFlat: null,
+      search: ""
     };
   }
 
@@ -18,34 +22,73 @@ class App extends Component {
     .then(response => response.json())
     .then(data => {
       this.setState({
-        flats: data
+        flats: data,
+        allFlats: data
       })
     })
 
   }
 
+  selectFlat = (flat) => {
+    console.log(flat)
+    this.setState({
+      selectedFlat: flat
+    })
+  }
+
+  handleSearch = (event) => {
+    this.setState ({
+      search: event.target.value,
+      flats: this.state.allFlats.filter((flat) => new RegExp(event.target.value, "i").exec(flat.name))
+    })
+  }
 
   render() {
-    const center = {
+    let center = {
       lat: 48.8566,
       lng: 2.3522
     }
+
+    if (this.state.selectedFlat) {
+      center = {
+        lat: this.state.selectedFlat.lat,
+        lng: this.state.selectedFlat.lng
+      }
+    }
+
     return (
       <div className="app">
         <div className="main">
           <div className="search">
+            <input
+              type="text"
+              placeholder="Search..."
+              value={this.state.search}
+              onChange={this.handleSearch} />
           </div>
           <div className="flats">
             {this.state.flats.map((flat) => {
-              return <Flat flat={flat} />
+              return <Flat
+                key={flat.name}
+                flat={flat}
+                selectFlat={this.selectFlat} />
             })}
           </div>
         </div>
         <div className="map">
           <GoogleMapReact
             center={center}
-            zoom={11}
+            zoom={13}
           >
+          {this.state.flats.map((flat) => {
+              return <Marker
+                key={flat.name}
+                lat={flat.lat}
+                lng={flat.lng}
+                text={flat.price}
+                selected={flat === this.state.selectedFlat}
+                />
+            })}
           </GoogleMapReact>
         </div>
       </div>
